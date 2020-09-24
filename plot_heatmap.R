79b5535d56cdcc1afb4ea6d8f6e6061184855b0f
Marion-Mai <57678444+Marion-Mai@users.noreply.github.com>
2020-09-24 20:31:19 +0200

    Update plot_heatmap.R

--- plot_heatmap.R
+++ plot_heatmap.R
@@ -109,6 +109,7 @@ m <- melted_netmtri %>%
   theme_minimal()+
   theme(plot.title = element_text(face = "bold"), # , hjust = -0.3
         plot.subtitle = element_text(face = "italic"),
+        axis.title = element_text(color = "#4c4c4c"),
         axis.text.x = element_text(angle = 45, vjust = 1,
                                    size = 10, hjust = 1),
         axis.text.y = element_text(size = 10),
